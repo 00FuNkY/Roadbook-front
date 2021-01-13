f7a4353a4691f31a7c9c749da715ad361e9d4186
De Zaldua Samuel <samuel.dezaldua@gmail.com>
2021-01-13 14:54:30 +0100

    created cities list and hover effect

---
+++ src/assets/cities_images.js
@@ -0,0 +1,21 @@
+export const data = [{
+  name: 'AMSTERDAM',
+  url: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y2l0eXxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=500&q=60',
+},
+{
+  name: 'ANDORRE-LA-VIEILLE',
+  url: 'https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+},
+{
+  name: 'ATHENES',
+  url: 'https://images.unsplash.com/photo-1514565131-fce0801e5785?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+},
+{
+  name: 'BELGRADE',
+  url: 'https://www.google.com/search?q=city+image&sxsrf=ALeKk00dnNHWRBO9cCASu1NggRaetR0xRA:1610542791002&tbm=isch&source=iu&ictx=1&fir=BAcDmkIQSUANzM%252CKcee-H1PB3XMBM%252C_&vet=1&usg=AI4_-kS6n-gxk88u_YFrrxNyQPfAXqTLTw&sa=X&ved=2ahUKEwjB3c-3-5juAhVFQhoKHU3hCzYQ9QF6BAgLEAE#imgrc=BAcDmkIQSUANzM'
+},
+{
+  name: 'BERLIN',
+  url: 'https://www.google.com/search?q=city+image&sxsrf=ALeKk00dnNHWRBO9cCASu1NggRaetR0xRA:1610542791002&tbm=isch&source=iu&ictx=1&fir=KsqmrjsrnScraM%252CIgPdAdX_6UbCuM%252C_&vet=1&usg=AI4_-kQhosADjZfnrUivO4B-HAvexKu02Q&sa=X&ved=2ahUKEwjB3c-3-5juAhVFQhoKHU3hCzYQ9QF6BAgQEAE#imgrc=KsqmrjsrnScraM'
+}
+]
--- src/components/CitiesList.jsx
+++ src/components/CitiesList.jsx
@@ -1,24 +1,36 @@
+import { useRef, useState } from "react";
 import styled from "styled-components";
+import { data } from '../assets/cities_images'
 
 const CitiesList = () => {
-  const cities = [
-    'AMSTERDAM',
-    'ANDORRE-LA-VIEILLE',
-    'ATHENES',
-    'BELGRADE', 
-    'BERLIN'
-  ]
+  const cityName = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
+  const [index, setIndex] = useState(null)
 
-  const mouseEnterHandler = () => {
-    console.log('hey');
+  const mouseEnterHandler = (i) => {
+    setIsHovered(true)
+    setIndex(i)
+  }
+  const mouseLeaveHandler = () => {
+    setIsHovered(false)
+    setIndex(-1)
   }
 
   return (
+    <StyledContainer>
     <StyledList> 
-    {cities.map((city, i) => {
-      return <h1 onMouseEnter={mouseEnterHandler}>{city}</h1>
+    {data.map((city, i) => {
+      return <h1 
+      onMouseEnter={() => mouseEnterHandler(i)}
+      onMouseLeave={mouseLeaveHandler}
+      className={isHovered ? 'isHovered' : ''}
+      ref={cityName}
+      key={i}
+      >{city.name}</h1>
     })}
     </ StyledList>
+    <StyledImage src={data[index]?.url}/>
+    </StyledContainer>
    );
 }
 
@@ -27,10 +39,29 @@ const StyledList = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
+  mix-blend-mode: difference;
   h1{
     font-size: 5vh;
     font-weight: 100;
+    width: 50vw;
+    opacity: 0.5;
+    transition: .35s;
+    &:hover{
+      opacity: 1;
+    }
   }
+  .isHovered{
+    cursor: alias;
+    opacity: 0.1;
+  }
+`
+
+const StyledImage = styled.img`
+
+`
+
+const StyledContainer = styled.div`
+  display: flex;
 `
  
 export default CitiesList;
